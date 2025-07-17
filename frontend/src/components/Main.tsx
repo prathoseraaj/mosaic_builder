faeede978a617d7adfa58cd373ff20ff0862e5c1
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-07-17 15:48:23 +0530

    fix: audio and video input field

--- frontend/src/components/Main.tsx
+++ frontend/src/components/Main.tsx
@@ -20,25 +20,58 @@ const Main = () => {
               placeholder="Describe Your Memory in Detail."
             />
           </div>
-          <div className="flex-col">
-            <h1 className="font-bold">Audio</h1>
-            <input
-              type="file"
-              className="w-full bg-gray-100 p-2 mt-1 rounded"
-              placeholder="Describe Your Memory in Detail."
-            />
+          <div className="flex-col mt-5">
+            <h1 className="font-bold mb-1">Audio</h1>
+            <div className="flex flex-col items-center gap-6 rounded-lg border-2 border-dashed border-[#e4e0dd] px-5 py-10">
+              <div className="flex max-w-[480px] flex-col items-center gap-2">
+                <p className="text-[#171412] text-lg font-bold leading-tight tracking-[-0.015em] max-w-[480px] text-center">
+                  Drag and drop audio file here
+                </p>
+                <p className="text-[#171412] text-sm font-normal leading-normal max-w-[480px] text-center">
+                  Or click to browse your audio file
+                </p>
+              </div>
+
+              <input
+                type="file"
+                id="audioInput"
+                accept="audio/*"
+                className="hidden"
+              />
+
+              <label
+                htmlFor="audioInput"
+                className="flex min-w-[84px] max-w-[480px] cursor-pointer items-center justify-center overflow-hidden rounded-lg h-10 px-4 bg-[#f4f2f1] text-[#171412] text-sm font-bold leading-normal tracking-[0.015em]"
+              >
+                <span className="truncate">Upload</span>
+              </label>
+            </div>
           </div>
         </div>
-        <div className="flex-col mt-5">
-          <h1 className="font-bold">Image</h1>
-          <input
-            type="file"
-            className="w-full bg-gray-100 h-[200px] p-2 mt-1 rounded"
-            placeholder="Describe Your Memory in Detail."
-          />
+        <div className="flex flex-col mt-5">
+          <h1 className="font-bold mb-1">Image</h1>
+          <div className="flex flex-col items-center gap-6 rounded-lg border-2 border-dashed border-[#e4e0dd] px-5 py-10">
+            <div className="flex max-w-[480px] flex-col items-center gap-2">
+              <p className="text-[#171412] text-lg font-bold leading-tight tracking-[-0.015em] max-w-[480px] text-center">
+                Drag and drop files here
+              </p>
+              <p className="text-[#171412] text-sm font-normal leading-normal max-w-[480px] text-center">
+                Or click to browse your files
+              </p>
+            </div>
+            <input type="file" id="fileInput" className="hidden" />
+            <label
+              htmlFor="fileInput"
+              className="flex min-w-[84px] max-w-[480px] cursor-pointer items-center justify-center overflow-hidden rounded-lg h-10 px-4 bg-[#f4f2f1] text-[#171412] text-sm font-bold leading-normal tracking-[0.015em]"
+            >
+              <span className="truncate">Upload</span>
+            </label>
+          </div>
         </div>
         <div className="w-full mt-3 flex justify-center items-center">
-          <button className="border px-2 py-1 rounded bg-black text-white">Process it!</button>
+          <button className="border px-2 py-1 rounded bg-black text-white">
+            Process it!
+          </button>
         </div>
       </div>
     </div>
