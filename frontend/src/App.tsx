db711ecf28e6d50ec548a2f46f2684d87454d7c7
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-07-17 19:59:07 +0530

    fix: overflow as to remove the scroll bar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,7 @@ import Navbar from "./components/Navbar"
 
 const App = () => {
   return (
-    <div className="h-[100vh] w-full">
+    <div className="h-[100vh] w-full overflow-hidden">
       <Navbar/>
       <hr  className="mt-2 text-gray-200 font-bold"/>
       <Main/>
